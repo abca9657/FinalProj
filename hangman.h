1de5356934a6e1dacc06a1c7575fc99d9da178b9
abca9657 <abca9657@colorado.edu>
2015-04-23 12:23:21 -0600

    Update hangman.h

--- hangman.h
+++ hangman.h
@@ -1,3 +1,4 @@
+
 //hangman header file
 
 #include <iostream>
@@ -10,13 +11,35 @@ using namespace std;
 #ifndef HANGMAN_H
 #define HANGMAN_H
 
+//these are the nodes to store the words and points
+struct item
+{
+    string word;
+    int score;
+    //item* next;
+    item* next;
+
+    item(){};
+
+    item(string in_word, int in_score)
+    {
+        word = in_word;
+        score = in_score;
+    }
+};
+
+
 class Hangman
 {
 public:
+    Hangman();
 	void drawHangman(int count);
+	int Hash(string key);           //This returns a hash number for a word
+	void addItem(string word, int score);      //This adds a word and score to a node and puts it in the hashtable
 
 private:
-    int test;
+    static const int tableSize = 10;
+    item* HashTable[tableSize];     //This is the hashtable with all of our nodes in it
 
 };
 
