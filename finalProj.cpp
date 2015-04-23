36879e0ad39fe1a4d51b25399c95e95815447bd4
alal4055 <alal4055@colorado.edu>
2015-04-23 11:40:47 -0600

    Update finalProj.cpp

--- finalProj.cpp
+++ finalProj.cpp
@@ -6,3 +6,19 @@ int main()
 {
   cout<<"test"<<endl;
 }
+
+
+void readFileIn(){
+    std::string line;
+    std::string words[350];
+    int wordCount=0;
+    std::ifstream myfile;
+    myfile.open("words.txt");   //opens the file
+    if (myfile.is_open())   //goes through this code since file is open
+    {
+        while(getline(myfile, line)) {    //get each line in the file and puts them in the string line.
+            words[wordCount] = line.substr();
+            wordCount++;
+        }
+    }
+}
