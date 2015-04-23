d07210fc7749611471c6662151ac23a215b37ee5
abca9657 <abca9657@colorado.edu>
2015-04-23 13:48:46 -0600

    Update finalProj.cpp

--- finalProj.cpp
+++ finalProj.cpp
@@ -5,36 +5,12 @@
 
 using namespace std;
 
-int Hangman::main()
+int main()
 {
-  readFileIn();
+    Hangman obj;
+
+    string file="words.txt";
+  obj.readFileIn(file);
   cout<<"test"<<endl;
 }
 
-
-void Hangman::readFileIn(){
-    std::string line;
-    std::string words[350];
-    std::string stringScore[350];
-    int wordCount=0;
-    std::ifstream myfile;
-    myfile.open("words.txt");   //opens the file
-    if (myfile.is_open())   //goes through this code since file is open
-    {
-        while(getline(myfile, line)) {    //get each line in the file and puts them in array.
-            int index = line.find(",");
-            words[wordCount] = line.substr(0,index);
-            line = line.substr(index+1);
-            stringScore[wordCount]=line.substr();
-            wordCount++;
-        }
-    }
-    int scoreArray[350]   
-    for(int i=0; i<wordCount; i++){ //changes strings to int
-      scoreArray[i] = atoi(stringScore[i].c_str());
-    }
-    Hangman h;
-    for(int i=0;i<wordCount; i++){  //puts items into hashtable
-    h.addItem(words[i], scoreArray[i])
-    }
-}
