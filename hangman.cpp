af00d8693a2e80ca73b9af0adf372d643868c9b2
abca9657 <abca9657@colorado.edu>
2015-04-23 12:22:07 -0600

    Update hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -8,10 +8,67 @@
 
 using namespace std;
 
-/*Hangman::Hangman()
+Hangman::Hangman()
 {
+    for(int i=0; i<tableSize; i++)
+    {
+        HashTable[i] = new item;
+        HashTable[i]->word = "empty";
+        HashTable[i]->score = 0;
+        HashTable[i]->next = NULL;
+    }
+}
 
-}*/
+
+//This function changes a word (key) to a number for the hash table
+int Hangman::Hash(string key)
+{
+    //key is a word like "Paul" to add to the table
+    int hsh = 0;
+    int index;
+
+
+    //add all the characters up to get a number
+    for(int i=0; i<key.length(); i++)
+    {
+        hsh = hsh+(int)key[i]; //multiply this number to get a new way of storing
+    }
+
+    index = hsh % tableSize;
+        // taking hsh value divided by 100 (result = remainder)
+    return index;
+    //Divide given number by table size and use
+    //remainder as index for hash table
+}
+
+
+//This function adds a given word and score to the hash table
+void Hangman::addItem(string word, int score)
+{
+    //stores index of name
+    int index = Hash(word);
+
+    if(HashTable[index]->word == "empty")
+    {
+        HashTable[index]->word = word;
+        HashTable[index]->score = score;
+        HashTable[index]->next = NULL;
+    }
+    else
+    {   //store item in the node
+        item* ptr = HashTable[index];
+        item* n = new item;
+        n->word = word;
+        n->score = score;
+        while(ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        //ptr now pointing to last item in list
+        ptr->next = n;
+    }
+}
+
 
 //This will draw the hangman based on what guess they're on (ie, first, second, third, etc.)
 void Hangman::drawHangman(int count)
@@ -53,3 +110,4 @@ void Hangman::drawHangman(int count)
 	else
 		count = 7;
 }
+
