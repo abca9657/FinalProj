65f711ed0247b86f0de3f14b6c27aefdb4cf6c97
user <abca9657>
2015-04-23 11:32:34 -0600

    Hangman Header1

--- hangman.h
+++ hangman.h
@@ -1,1 +1,23 @@
 //hangman header file
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+
+using namespace std;
+
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+class Hangman
+{
+public:
+	void drawHangman(int count);
+
+private:
+	
+{
+
+
+#endif
