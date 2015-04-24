7cb3a1d4729ac7ed0d19d8c4dedfcec231d30c77
alal4055 <alal4055@colorado.edu>
2015-04-24 00:18:35 -0600

    Update finalProj.cpp

--- finalProj.cpp
+++ finalProj.cpp
@@ -7,10 +7,13 @@ using namespace std;
 
 int main()
 {
-    Hangman obj;
-
+      Hangman obj;
     string file="words.txt";
   obj.readFileIn(file);
-  cout<<"test"<<endl;
+
+  char guess;
+  cin>>guess;
+    string trackGuess = obj.checkIfGuessed("banana", guess,"ba_a_a");
+cout<<trackGuess;
 }
 
